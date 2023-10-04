87b1eac326c4ae5c378f2a406602a31ea6ca9f4c
KIrillweb2023 <129139006+KIrillweb2023@users.noreply.github.com>
2023-10-04 23:57:23 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,17 +24,21 @@ menuScroll(open, close, menu, links);
 const logo = document.querySelectorAll('.logo');
 const hamburger = document.querySelector('.ham');
 const closeImg = document.querySelector('.close-add');
-function theme(checkbox, restangle, body){
+const logoCompany = document.querySelector('.logo__company');
+function theme(checkbox, restangle, body, logoCompany){
     checkbox.addEventListener('click', (e) =>{
         restangle.classList.toggle('active');
         if(restangle.classList.contains('active')){
             //body.setAttribute('class', ''); 
             body.classList.add('active');
-          
+            logoCompany.setAttribute('src', './icons/slider-tabs/tinkoff-white.svg');
         } else {
             body.classList.remove('active');
+            logoCompany.setAttribute('src', './icons/slider-tabs/tinkoff-dark.svg');
         }
     });
 }
-theme(document.querySelector('.restangle'), document.querySelector('.restangle-child'), document.body);
+theme(document.querySelector('.restangle'), document.querySelector('.restangle-child'), document.body, logoCompany);
+
+
 
